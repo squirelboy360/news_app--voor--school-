99a6dbc662429992542392692d9c2a65aa092b9c
Tahiru Agbanwa <130235676+squirelboy360@users.noreply.github.com>
2025-06-19 15:10:57 +0200

    update

--- docs/presentatie.md
+++
@@ -1,5 +0,0 @@
-# Presentatie
-
-- Demo gepland in week 7
-- Product gepresenteerd aan begeleider + feedback gevraagd
-- Gebruikersverhaal per feature toegelicht
--- sekai_news/lib/main.dart
+++ sekai_news/lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:sekai_news/screens/main_screens/main_screen.dart';
 
 void main() async {
@@ -16,23 +16,35 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      locale: const Locale('nl', 'NL'), // Nederlandse locale
+      locale: const Locale('nl', 'NL'),
       localizationsDelegates: const [
         GlobalMaterialLocalizations.delegate,
         GlobalWidgetsLocalizations.delegate,
         GlobalCupertinoLocalizations.delegate,
       ],
       supportedLocales: const [
-        Locale('nl', 'NL'), // Nederlands
-        Locale('en', 'US'), // English fallback
+        Locale('nl', 'NL'),
+        Locale('en', 'US'),
       ],
       color: Colors.white54,
       theme: ThemeData(
         splashColor: Colors.transparent,
         splashFactory: NoSplash.splashFactory,
         highlightColor: Colors.transparent,
-        bottomNavigationBarTheme:const BottomNavigationBarThemeData(elevation: 0,backgroundColor: Colors.white,type: BottomNavigationBarType.shifting,unselectedItemColor: Colors.grey,landscapeLayout: BottomNavigationBarLandscapeLayout.centered,showSelectedLabels: false,selectedIconTheme: IconThemeData(size: 50)),
-        appBarTheme: const AppBarTheme(backgroundColor: Colors.blueAccent,foregroundColor: Colors.black,elevation: 0,centerTitle: true,),
+        bottomNavigationBarTheme: const BottomNavigationBarThemeData(
+            elevation: 0,
+            backgroundColor: Colors.white,
+            type: BottomNavigationBarType.shifting,
+            unselectedItemColor: Colors.grey,
+            landscapeLayout: BottomNavigationBarLandscapeLayout.centered,
+            showSelectedLabels: false,
+            selectedIconTheme: IconThemeData(size: 50)),
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Colors.blueAccent,
+          foregroundColor: Colors.black,
+          elevation: 0,
+          centerTitle: true,
+        ),
       ),
       home: const MainNavScreen(),
     );
